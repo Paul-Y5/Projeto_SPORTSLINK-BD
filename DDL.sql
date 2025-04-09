720f4b02f9a6fa1abf40d5e685f58b290f05d539
Paul-Y5 <paulo.c50@ua.pt>
2025-04-09 23:51:09 +0100

    DDL_Pronto

--- DDL.sql
+++ DDL.sql
@@ -15,29 +15,16 @@ CREATE TABLE Utilizador (
   PRIMARY KEY ID
 );
 
+
 CREATE TABLE Jogador (
-  ID_Utilizador     INT,
+  ID                INT,
   Idade             INT,
   Descricao         VARCHAR(MAX),
 
-  PRIMARY KEY ID_Utilizador,
-  FOREIGN KEY ID_Utilizador REFERENCES Utilizador(ID)
+  PRIMARY KEY ID,
+  FOREIGN KEY ID REFERENCES Utilizador(ID)
 );
 
-CREATE TABLE Desporto (
-  ID_Desporto INT PRIMARY KEY,
-  Nome VARCHAR(50) UNIQUE NOT NULL
-);
-
-
-CREATE TABLE Desporto_Jogador (
-  ID_Jogador INT,
-  ID_Desporto INT,
-
-  PRIMARY KEY (ID_Jogador, ID_Desporto),
-  FOREIGN KEY (ID_Jogador) REFERENCES Jogador(ID_Jogador),
-  FOREIGN KEY (ID_Desporto) REFERENCES Desporto(ID_Desporto)
-);
 
 CREATE TABLE Arrendador (
   ID_Arrendador     INT,
@@ -50,12 +37,14 @@ CREATE TABLE Arrendador (
 
 );
 
+
 CREATE TABLE Mapa (
   ID            INT,
   Ultimo_update TIMESTAMP,
 
   PRIMARY KEY ID
 );
+
 
 CREATE TABLE ModExib_Mapa (
   ID_Mapa           INT,
@@ -64,6 +53,7 @@ CREATE TABLE ModExib_Mapa (
   PRIMARY KEY (ID_Mapa, Modo_exib),
   FOREIGN KEY ID_Mapa REFERENCES Mapa(ID)
 );
+
 
 CREATE TABLE Ponto (
   ID            INT,
@@ -74,6 +64,7 @@ CREATE TABLE Ponto (
   PRIMARY KEY (ID, ID_Mapa),
   FOREIGN KEY ID_Mapa REFERENCES Mapa(ID)
 );
+
 
 CREATE TABLE Campo (
   ID            INT,
@@ -88,6 +79,7 @@ CREATE TABLE Campo (
   FOREIGN KEY ID_Ponto REFERENCES Ponto(ID)
 );
 
+
 CREATE TABLE Campo_Priv (
   ID_Campo            INT,
   ID_Arrendador       INT,
@@ -98,6 +90,7 @@ CREATE TABLE Campo_Priv (
 
 );
 
+
 CREATE TABLE Campo_Pub (
   ID_Campo              INT,
   Entidade_publica_resp VARCHAR(256),
@@ -107,18 +100,142 @@ CREATE TABLE Campo_Pub (
 );
 
 
+CREATE TABLE Imagem (
+  [URL]              VARCHAR(512)
+
+  PRIMARY KEY [URL]
+);
 
 
+CREATE TABLE IMG_Campo (
+  ID_Campo           INT,
+  [URL]              VARCHAR(512),
+
+  PRIMARY KEY (ID_Campo, [URL]),
+  FOREIGN KEY ID_Campo REFERENCES Campo(ID),
+  FOREIGN KEY [URL] REFERENCES Imagem([URL])
+);
 
 
+CREATE TABLE IMG_Perfil (
+  ID_Utilizador         INT,
+  [URL]              VARCHAR(512),
+
+  PRIMARY KEY (ID_Utilizador, [URL]),
+  FOREIGN KEY ID_Utilizador REFERENCES Utilizador(ID),
+  FOREIGN KEY [URL] REFERENCES Imagem([URL])
+);
 
 
+CREATE TABLE Partida (
+  ID                 INT,
+  ID_Campo           INT,
+  no_jogadores       INT,
+  Data_Hora          DATETIME,
+  Duracao            INT,
+  Resultado          VARCHAR(50),
+
+  PRIMARY KEY ID,
+  FOREIGN KEY ID_Campo REFERENCES Campo(ID)
+);
 
 
+CREATE TABLE Jogador_joga (
+  ID_Partida         INT,
+  ID_Jogador         INT,
+
+  PRIMARY KEY (ID_Partida, ID_Jogador),
+  FOREIGN KEY ID_Partida REFERENCES Partida(ID),
+  FOREIGN KEY ID_Jogador REFERENCES Jogador(ID)
+);
 
 
+CREATE TABLE Reserva (
+  ID                 INT,
+  ID_Campo           INT,
+  ID_Jogador         INT,
+  Data_Hora          DATETIME,
+  Descricao         VARCHAR(MAX)
+
+  PRIMARY KEY ID,
+  FOREIGN KEY ID_Campo REFERENCES Campo(ID),
+  FOREIGN KEY ID_Jogador REFERENCES Jogador(ID)
+);
 
 
+CREATE TABLE Rating (
+  ID_Avaliador      INT,
+  Data_Hora         DATETIME,
+  Comentario        VARCHAR(MAX),
+  Avaliacao         INT,
+
+  PRIMARY KEY ID_Avaliador,
+  FOREIGN KEY ID_Avaliador REFERENCES Jogador(ID)
+);
+
+
+CREATE TABLE Rating_Campo (
+  ID_Avaliador       INT,
+  ID_Campo           INT,
+
+  PRIMARY KEY (ID_Campo, ID_Avaliador),
+  FOREIGN KEY ID_Campo REFERENCES Campo(ID),
+  FOREIGN KEY ID_Avaliador REFERENCES Rating(ID_Avaliador)
+);
+
+
+CREATE TABLE Rating_Jogador (
+  ID_Jogador         INT,
+  ID_Avaliador       INT,
+
+  PRIMARY KEY (ID_Jogador, ID_Avaliador),
+  FOREIGN KEY ID_Jogador REFERENCES Jogador(ID),
+  FOREIGN KEY ID_Avaliador REFERENCES Rating(ID_Avaliador)
+);
+
+
+CREATE TABLE Precario (
+  ID_Campo           INT,
+  preco              DECIMAL(10,2),
+
+  PRIMARY KEY ID_Campo,
+  FOREIGN KEY ID_Campo REFERENCES Campo_Priv(ID_Campo)
+);
+
+
+CREATE TABLE Chat_Live (
+  ID_Partida         INT,
+  Titulo             VARCHAR(256)
+
+  PRIMARY KEY ID_Partida,
+  FOREIGN KEY ID_Partida REFERENCES Partida(ID)
+);
+
+
+CREATE TABLE Desporto (
+  ID_Desporto INT PRIMARY KEY,
+  Nome VARCHAR(50) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE Desporto_Jogador (
+  ID_Jogador INT,
+  ID_Desporto INT,
+
+  PRIMARY KEY (ID_Jogador, ID_Desporto),
+  FOREIGN KEY (ID_Jogador) REFERENCES Jogador(ID),
+  FOREIGN KEY (ID_Desporto) REFERENCES Desporto(ID_Desporto)
+);
+
+
+CREATE TABLE Desporto_Campo (
+  ID_Desporto        INT,
+  ID_Campo           INT,
+
+  PRIMARY KEY (ID_Desporto, ID_Campo),
+  FOREIGN KEY ID_Desporto REFERENCES Desporto(ID),
+  FOREIGN KEY ID_Campo REFERENCES Campo(ID)
+);
 
 
 CREATE TABLE Jogador_Amizade (
@@ -128,4 +245,13 @@ CREATE TABLE Jogador_Amizade (
   FOREIGN KEY (ID_J1) REFERENCES Jogador(ID),
   FOREIGN KEY (ID_J2) REFERENCES Jogador(ID),
   CHECK (ID_J1 < ID_J2)  -- Garante que não há duplicações invertidas
+);
+
+
+CREATE TABLE Met_Paga_Arrendador (
+  ID_Arrendador       INT,
+  Met_pagamento       VARCHAR(50),
+
+  PRIMARY KEY (ID_Arrendador, Met_pagamento),
+  FOREIGN KEY ID_Arrendador REFERENCES Arrendador(ID_Arrendador)
 );
